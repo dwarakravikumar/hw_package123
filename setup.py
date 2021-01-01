2e994b85172737c593d932627b89aa7e371c36b3
dwarakravikumar <64508461+dwarakravikumar@users.noreply.github.com>
2020-12-31 23:27:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.0'
+VERSION = '1.0'
 PACKAGE_NAME = 'hw_package123'
 AUTHOR = 'dwarakravikumar'
 AUTHOR_EMAIL = ''
