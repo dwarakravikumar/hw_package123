3fbb52fe292a556a1155867db914e45f0f1c517a
dwarakravikumar <64508461+dwarakravikumar@users.noreply.github.com>
2020-12-31 23:20:43 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'hw_package',
+    packages = ['hw_package'],
+    version = 'v1.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'Dwarak Ravikumar',
+    author_email = 'dwarak.ravikumar@gmail.com',
+    url = 'https://github.com/dwarakravikumar/dwarak_trial',
+    download_url = '',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
