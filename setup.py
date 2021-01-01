c35565fe7c2ea47eb5dd9134a05c1ed7fe02ab91
dwarakravikumar <64508461+dwarakravikumar@users.noreply.github.com>
2020-12-31 23:22:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup, find_packages
 
-setup(
-    name = 'hw_package',
-    packages = ['hw_package'],
-    version = 'v1.1',  # Ideally should be same as your GitHub release tag varsion
-    description = 'description',
-    author = 'Dwarak Ravikumar',
-    author_email = 'dwarak.ravikumar@gmail.com',
-    url = 'https://github.com/dwarakravikumar/dwarak_trial',
-    download_url = '',
-    keywords = ['tag1', 'tag2'],
-    classifiers = [],
-)
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'your_package'
+AUTHOR = 'You'
+AUTHOR_EMAIL = 'you@email.com'
+URL = 'https://github.com/you/your_package'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Describe your package in one sentence'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
